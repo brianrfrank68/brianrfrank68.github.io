00e02008226d726a97f4b40928303df89e073372
Brian Frank <brianrfrank@yahoo.com>
2024-01-05 08:07:12 -0600

    analysis

--- Files/Batter_Draft.sql
+++ Files/Batter_Draft.sql
@@ -1,6 +1,4 @@
-
-SELECT 
-pitching.Name,
+SELECT Batting.Name,
 roster.flb_team_init,
        Age,
        Dynasty_October.Rank,
@@ -14,48 +12,42 @@ roster.flb_team_init,
        Dynasty_Prospect_March."POS.",
        Dynasty_Prospect_March."SEC. POS.",
        Tm,
-       Pitching_Value.WAR,
-       W,
-       L,
-       ERA,
+       Batting_Value.WAR,
        G,
-       GS,
-       GF,
-       CG,
-       SHO,
-       SV,
-       IP,
-       H,
+       AB,
        R,
-       ER,
+       H,
+       "2B",
+       "3B",
        HR,
+       RBI,
+       SB,
+       CS,
        BB,
-       IBB,
        SO,
-       FIP,
-       WHIP,
-       H9,
-       HR9,
-       BB9,
-       SO9,
-       "SO/W",
-       pitching."Name-additional"
-  FROM pitching
-LEFT OUTER JOIN roster on pitching."Name-additional" = roster.player_id
-LEFT OUTER JOIN Pitching_Value on pitching."Name-additional" = Pitching_Value."Name-additional"
-LEFT OUTER JOIN Fielding on Fielding."Name-additional" = pitching."Name-additional"
-LEFT OUTER JOIN player_list on player_list.player_id = pitching."Name-additional"
+       BA,
+       OBP,
+       SLG,
+       OPS,
+       "Pos Summary",
+       --Fielding."Pos Summary",
+       "Name-additional"
+  FROM Batting 
+LEFT OUTER JOIN roster on Batting."Name-additional" = roster.player_id
+LEFT OUTER JOIN Batting_Value on Batting."Name-additional" = Batting_Value."Name-additional"
+LEFT OUTER JOIN Fielding on Fielding."Name-additional" = Batting."Name-additional"
+LEFT OUTER JOIN player_list on player_list.player_id = Batting."Name-additional"
 LEFT OUTER JOIN Dynasty_October on (player_list.player_name = Dynasty_October.Player OR Dynasty_October.Player = player_list.alt_name)
 LEFT OUTER JOIN Dynasty_Prospect_October on (player_list.player_name = Dynasty_Prospect_October.Player OR Dynasty_Prospect_October.Player = player_list.alt_name)
 LEFT OUTER JOIN Dynasty_Prospect_March on (player_list.player_name = Dynasty_Prospect_March.Player OR Dynasty_Prospect_March.Player = player_list.alt_name)
-  --inner join fielding on fielding.player_id = pitchers.player_id
- WHERE
-  roster.flb_team_init IS NULL
-  
+WHERE 
+(
+Batting."Pos Summary" LIKE '9%'
+OR Batting."Pos Summary" LIKE '*9%'
+OR Batting."Pos Summary" LIKE '/9%'
+)
+AND roster.flb_team_init IS NULL
 ORDER BY CAST(Dynasty_October.Rank AS INT) ASC NULLS LAST, 
 CAST(Dynasty_Prospect_October.Rank AS INT) ASC NULLS LAST, 
 CAST(Dynasty_Prospect_March.Rank AS INT) ASC NULLS LAST, 
-Pitching_Value.WAR DESC, pitching."Name-additional"
-
-  
-
+Batting_Value.WAR DESC, Batting."Name-additional"
